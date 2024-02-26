94cf18e5d5d95c387a9677ac9a66ad607b2fda42
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-02-26 15:39:50 +0000

    Add new colors and styles to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,35 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          500: "#42BBFF",
+          800: "#0C3247",
+          900: "#42BBFF1A",
+        },
+        black: {
+          600: "#2E3757",
+          700: "#1D2032",
+          800: "#131625",
+          900: "#10121E",
+        },
+        white: {
+          100: "#FFFFFF",
+          300: "#ADB3CC",
+          500: "#55597D",
+        },
+        purle: {
+          500: "#9542FF",
+          900: "#9542FF1A",
+        },
+        green: {
+          400: "#68D1BF",
+          500: "#42FF77",
+          900: "#42FF771A",
+        },
+      },
       fontFamily: {
-        inter: ["Inter", "sans-serif"], // Assuming 'Inter' is available in your project
+        inter: ["Inter", "sans-serif"], 
       },
       fontSize: {
         custom: ["32px", "40px"], // The second value is for line-height
@@ -18,7 +45,11 @@ const config: Config = {
         custom_3: ["10px", "12px"],
       },
       letterSpacing: {
-        custom: "-0.02em", // Adjust based on visual similarity
+        custom: "-0.02em", 
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to right, #43B7FE 100%, #4F48E6 100%)",
       },
     },
   },
