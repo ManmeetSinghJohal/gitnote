4a72ec34133d857ac7297e79de96559e1fd50563
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-06 22:24:06 +0000

    Add postTags constant array

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,74 @@
-// contains the app’s constant variables
+export const postTags = [
+  {
+    value: "api",
+    label: "API",
+  },
+  {
+    value: "authentication",
+    label: "Authentication",
+  },
+  {
+    value: "css ",
+    label: "CSS ",
+  },
+  {
+    value: "clerk",
+    label: "Clerk",
+  },
+  {
+    value: "javascript",
+    label: "JavaScript",
+  },
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "react",
+    label: "React",
+  },
+  {
+    value: "typescript",
+    label: "TypeScript",
+  },
+  {
+    value: "vercel",
+    label: "Vercel",
+  },
+  {
+    value: "zod",
+    label: "Zod",
+  },
+  {
+    value: "frontend",
+    label: "frontend",
+  },
+  {
+    value: "hooks",
+    label: "hooks",
+  },
+  {
+    value: "react-hook-form",
+    label: "react-hook-form",
+  },
+  {
+    value: "serverless",
+    label: "serverless",
+  },
+  {
+    value: "tailwindcss",
+    label: "tailwindcss",
+  },
+  {
+    value: "yup",
+    label: "yup",
+  },
+  {
+    value: "zustand",
+    label: "zustand",
+  },
+  {
+    value: "zustand-persist",
+    label: "zustand-persist",
+  },
+];
