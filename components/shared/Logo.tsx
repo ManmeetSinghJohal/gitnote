b60a6fcad771586fa73d272523a246c926c6a1c9
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-13 08:14:08 +0000

    Add link to dashboard in Logo component

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type LogoProps = {
@@ -9,10 +10,12 @@ type LogoProps = {
 
 const Logo = ({textSize, width, height}: LogoProps) => {
   return (
+    <Link href="/dashboard">
     <div className="flex gap-1">
       <Image src="/assets/icons/logo.svg" alt="logo" width={width} height={height} />
       <h3 className={`${textSize} font-bold text-white-100`}>GitNote</h3>
     </div>
+    </Link>
   );
 };
 
