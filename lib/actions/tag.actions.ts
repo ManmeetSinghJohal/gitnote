29a1b313e260a896a733d488fb41c1e5d9d475c3
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-17 11:01:52 +0000

    Refactor tag.actions.ts to return tags as a JSON string

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import Tag, { ITag } from "@/database/tag.model";
+import Tag from "@/database/tag.model";
 
 import { connectToDatabase } from "../mongoose";
 
@@ -8,7 +8,7 @@ export async function getTags() {
   try {
     await connectToDatabase();
     const tags = await Tag.find();
-    return tags;
+    return JSON.stringify(tags);
   } catch (error) {
     console.log("Error getting tags", error);
   }
@@ -22,11 +22,8 @@ export async function queryTags(tags: { value: string }[]): Promise<string[]> {
       let foundTag = await Tag.findOne({ value: tag.value });
       if (!foundTag) {
         foundTag = await Tag.create(tag);
-        console.log("newTagid", foundTag._id);
       }
       tagsArray.push(foundTag._id.toString())
-
-      console.log("tagsArray", tagsArray);
     }
   } catch (error) {
     console.log("Error querying tags", error);
