809f0b2a700749a3f02549775ff0365f7c85c2b6
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-24 17:25:16 +0000

    Add ownerId field to Tag model and set it when creating a new tag

--- database/tag.model.ts
+++ database/tag.model.ts
@@ -3,11 +3,13 @@ import { Schema, model, models, Document } from "mongoose";
 export interface ITag extends Document {
   value: string;
   label: string;
+  ownerId: Schema.Types.ObjectId;
 }
 
 const TagSchema = new Schema({
   value: { type: String, required: true, unique: true },
   label: { type: String, required: true },
+  ownerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const Tag = models.Tag || model("Tag", TagSchema);
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -4,6 +4,8 @@ import Tag from "@/database/tag.model";
 
 import { connectToDatabase } from "../mongoose";
 
+import { getActiveUser } from "./user.action";
+
 export async function getTags() {
   try {
     await connectToDatabase();
@@ -18,10 +20,16 @@ export async function queryTags(tags: { value: string }[]): Promise<string[]> {
   const tagsArray: string[] = [];
   try {
     await connectToDatabase();
+    const user = await getActiveUser();
     for (const tag of tags) {
+      console.log("tag", tag);
       let foundTag = await Tag.findOne({ value: tag.value });
       if (!foundTag) {
-        foundTag = await Tag.create(tag);
+           foundTag = await Tag.create({
+             value: tag.value,
+             label: tag.value,
+             ownerId: user.id,
+           });
       }
       tagsArray.push(foundTag._id.toString());
     }
