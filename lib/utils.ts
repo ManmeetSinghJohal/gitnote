a86f837a1e2b53df1f1387204a52cad00a65ce52
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-25 10:25:00 +0000

    Fix formatting and add new utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function getCreateTypeColor(createType: string) {
+  const colorMap: { [key: string]: string } = {
+    component: "text-purple-500 bg-purple-500/10",
+    workFlow: "text-primary1-500 bg-primary1-500/10",
+    knowledge: "text-green-500 bg-green-500/10",
+  };
+
+  return colorMap[createType] || "text-primary1-500 bg-primary1-500/10";
+}
+
+  export function capitalizeFirstLetter(string: string): string {
+    return string ? `${string.charAt(0).toUpperCase()}${string.slice(1)}` : "";
+  }
