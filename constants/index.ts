6eb81f8b6c4f92de6b4d5c3a7a3bf9299cbe7140
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-25 11:03:28 +0000

    Add createTypeBadge constants

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+import { CreateTypeBadge } from "../types";
+
+export const createTypeBadge: CreateTypeBadge[] = [
+  {
+    createType: "component",
+    textColor: "text-purple-500",
+    bgColor: "bg-purple-500/10",
+    name: "Component",
+  },
+  {
+    createType: "workFlow",
+    textColor: "text-primary1-500",
+    bgColor: "bg-primary1-500/10",
+    name: "WorkFlow",
+  },
+  {
+    createType: "knowledge",
+    textColor: "text-green-500",
+    bgColor: "bg-green-500/10",
+    name: "Knowledge",
+  },
+];
