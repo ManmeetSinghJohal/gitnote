986d1951424d17adb95cbc564c9d92c200594f76
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-25 11:03:18 +0000

    Add CreateTypeBadge interface to types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,11 @@
 export interface PostTags {
-    value: string;
-    label: string;
+  value: string;
+  label: string;
 }
 
+export interface CreateTypeBadge {
+  createType: string;
+  textColor: string;
+  bgColor: string;
+  name: string;
+}
