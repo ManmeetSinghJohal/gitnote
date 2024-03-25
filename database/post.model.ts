1e66883334a6f92e44d8b388062394cf55b573c0
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-25 13:52:41 +0000

    Update post.model.ts to include additional interfaces and schemas

--- database/post.model.ts
+++ database/post.model.ts
@@ -1,4 +1,5 @@
-import { Schema, models, model, Document } from "mongoose";
+import {Model,  Schema, models, model, Document } from "mongoose";
+import {ITag} from './tag.model';
 
 export interface IPost extends Document {
   title: string;
@@ -13,19 +14,41 @@ export interface IPost extends Document {
   createdAt: Date;
 }
 
-const PostSchema = new Schema({
+export interface ICheckListItem {
+  step_lesson: string;
+}
+
+const CheckListItem = new Schema<ICheckListItem>({
+  step_lesson: {type: String, required: true}
+});
+
+export interface IResource {
+  label: string;
+  resource: string;
+}
+
+const ResourceItem = new Schema<IResource>({
+  label: {type: String, required: true},
+  resource: {type: String, required: true}
+});
+
+export type IPostWithTags = Omit<IPost, "tags"> & {
+  tags: ITag[]
+}
+
+const PostSchema = new Schema<IPost>({
   title: { type: String, required: true },
   createType: { type: String, required: true },
   tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
   description: { type: String, required: true },
-  checkList: { type: Array, required: true },
+  checkList: [{ type: CheckListItem, required: true }],
   code: { type: String, required: false },
   content: { type: String, required: true },
-  resources: { type: Array, required: true },
+  resources: [{ type: ResourceItem, required: true }],
   ownerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: new Date(), required: true },
 });
 
-const Post = models.Post || model("Post", PostSchema);
+const Post = (models.Post as Model<IPost> ) || model<IPost>("Post", PostSchema);
 
 export default Post;
