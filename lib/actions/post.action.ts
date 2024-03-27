0406f2399777ff140d02e74cc75a9faa45974bd7
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-27 13:54:15 +0000

    Add ITag import and fix tags assignment in createPost function

--- lib/actions/post.action.ts
+++ lib/actions/post.action.ts
@@ -4,7 +4,7 @@ import { FilterQuery } from "mongoose";
 import { revalidatePath } from "next/cache";
 
 import Post, { IPost, IPostWithTags } from "@/database/post.model";
-import Tag from "@/database/tag.model";
+import Tag, { ITag } from "@/database/tag.model";
 
 import { connectToDatabase } from "../mongoose";
 
@@ -21,12 +21,12 @@ export async function createPost(params: CreatePostParams) {
     const {
       title,
       createType,
+      tags,
       description,
       checkList,
       code,
       content,
       resources,
-      tags,
     } = params;
 
     const tagIds = await queryTags(tags);
@@ -36,17 +36,16 @@ export async function createPost(params: CreatePostParams) {
     const post = await Post.create({
       title,
       createType,
+      tags: tagIds,
       description,
       checkList: checkListAsStringArray,
       code,
       content,
       resources,
-      tags: tagIds,
       ownerId: user.id,
     });
 
     revalidatePath("/dashboard");
-    console.log("Post created: ", post);
     return JSON.stringify(post);
   } catch (error) {
     console.error(error);
@@ -58,7 +57,7 @@ export async function getPosts() {
   try {
     await connectToDatabase();
     const user = await getActiveUser();
-    const filterObject: any = { ownerId: user.id };
+    const filterObject: FilterQuery<IPost> = { ownerId: user.id };
     const posts = await Post.find(filterObject);
     return posts;
   } catch (error) {
@@ -77,10 +76,12 @@ export async function getFilteredPosts(
 
     const user = await getActiveUser();
 
-    const filterObject: FilterQuery<IPost> = { ownerId: user.id };
+    const filterObject: FilterQuery<ITag | IPost> = { ownerId: user.id };
     if (tag) {
       const tagToUse = await Tag.findOne({ value: tag });
-      filterObject.tags = tagToUse._id;
+      if (tagToUse) {
+        filterObject.tags = tagToUse._id;
+      }
     }
 
     if (createType) {
