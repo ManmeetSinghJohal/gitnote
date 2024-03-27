8aaa8b967272ff428e7dead5c75d1d91e5a66525
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-27 13:53:49 +0000

    Update createTypeBadge to createTypeNames in constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,22 +1,16 @@
-import { CreateTypeBadge } from "../types";
+import { CreateTypeNames } from "../types";
 
-export const createTypeBadge: CreateTypeBadge[] = [
+export const createTypeNames: CreateTypeNames[] = [
   {
     createType: "component",
-    textColor: "text-purple-500",
-    bgColor: "bg-purple-500/10",
     name: "Component",
   },
   {
     createType: "workflow",
-    textColor: "text-primary1-500",
-    bgColor: "bg-primary1-500/10",
     name: "WorkFlow",
   },
   {
     createType: "knowledge",
-    textColor: "text-green-500",
-    bgColor: "bg-green-500/10",
     name: "Knowledge",
   },
 ];
