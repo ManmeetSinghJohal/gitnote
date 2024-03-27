daace65eaa47c54fd9b852355df7512dab40060b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-27 14:27:28 +0000

    Add text colors to createTypeNames

--- constants/index.ts
+++ constants/index.ts
@@ -3,14 +3,17 @@ import { CreateTypeNames } from "../types";
 export const createTypeNames: CreateTypeNames[] = [
   {
     createType: "component",
+    textColor: "text-blue-500",
     name: "Component",
   },
   {
     createType: "workflow",
+    textColor: "text-green-500",
     name: "WorkFlow",
   },
   {
     createType: "knowledge",
+    textColor: "text-yellow-500",
     name: "Knowledge",
   },
 ];
