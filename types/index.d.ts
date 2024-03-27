9a7016e275ee66b7fc93be795d439670886aa8b9
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-03-27 17:37:57 +0000

    Update createType to use POST_TYPE enum

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,12 @@
+import { POST_TYPE } from "@/database/post.model";
+
 export interface PostTags {
   value: string;
   label: string;
 }
 
 export interface CreateTypeNames {
-  createType: string;
+  createType: POST_TYPE;
   textColor: string;
   name: string;
 }
