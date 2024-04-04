db016223d95dac18c1cf8b6ef9d719ba1fccf5ba
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-04 20:26:12 +0100

    Add Prism CSS file for syntax highlighting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../styles/prism.css";
 import React from "react";
 
 import { AuthProvider } from "@/components/SessionProvider";
