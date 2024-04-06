74fa0489dac304d92caaabf47958ff9ce1384de1
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-06 14:15:19 +0100

    Refactor TinyMCEEditor component to improve readability and maintainability

--- components/shared/TinyMCEEditor.tsx
+++ components/shared/TinyMCEEditor.tsx
@@ -1,7 +1,7 @@
 import { Editor } from "@tinymce/tinymce-react";
 import React from "react";
 
-const TinyMCEEditor: React.FC = ({field, editorRef}) => {
+const TinyMCEEditor: React.FC = ({ field, editorRef }) => {
   return (
     <Editor
       apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
@@ -39,41 +39,40 @@ const TinyMCEEditor: React.FC = ({field, editorRef}) => {
         skin: "oxide-dark",
         content_css: "dark",
         setup: (editor) => {
-             const toWarningHtml = function () {
-               return `<div class="warning-box bg-[#20201E] border border-[#FFC700] rounded-[6px] p-3.5 text-white-100">Add warning info here....</div>&#8203;`;
-             };
+          const toWarningHtml = function () {
+            return `&#8203;<div class="warning-box" style="background-color: #20201E; border: 1px solid #FFC700; border-radius: 6px; padding: 14px; color: #FFFFFF;">Add warning info here....</div>&#8203;`;
+          };
 
-              const toErrorHtml = function () {
-                return `<div class="error-box bg-[#20201E] border border-[#FF4C3F] rounded-[6px] p-3.5 text-white-100">Add error info here....</div>&#8203;`;
-              };
+          const toErrorHtml = function () {
+            return `&#8203;<div class="error-box" style="background-color: rgba(255, 76, 63, 0.15); border: 1px solid #FF4C3F; border-radius: 6px; padding: 14px; color: #FFFFFF;">Add error info here....</div>&#8203;`;
+          };
 
-              const toInformationHtml = function () {
-                return `<div class="information-box bg-[#20201E] border border-[#2E3757] rounded-[6px] p-3.5 text-white-100">Add information info here....</div>&#8203;`;
-              };
+          const toInformationHtml = function () {
+            return `&#8203;<div class="information-box" style="background-color: #1D2032; border: 1px solid #2E3757; border-radius: 6px; padding: 14px; color: #FFFFFF;">Add information info here....</div>&#8203;`;
+          };
 
-            editor.ui.registry.addButton("warningButton", {
-              text: "Create Warning Label",
-              tooltip: "Create Warning Label",
-              onAction: function (_) {
-                editor.insertContent(toWarningHtml());
-              },
-            });
-            editor.ui.registry.addButton("errorButton", {
-              text: "Create Error Label",
-              tooltip: "Create Error Label",
-              onAction: function (_) {
-                editor.insertContent(toErrorHtml());
-              },
-            });
-             editor.ui.registry.addButton("informationButton", {
-               text: "Create Information Label",
-               tooltip: "Create Information Label",
-               onAction: function (_) {
-                 editor.insertContent(toInformationHtml());
-               },
-             });
-
-        }
+          editor.ui.registry.addButton("warningButton", {
+            text: "Create Warning Label",
+            tooltip: "Create Warning Label",
+            onAction: function (_) {
+              editor.insertContent(toWarningHtml());
+            },
+          });
+          editor.ui.registry.addButton("errorButton", {
+            text: "Create Error Label",
+            tooltip: "Create Error Label",
+            onAction: function (_) {
+              editor.insertContent(toErrorHtml());
+            },
+          });
+          editor.ui.registry.addButton("informationButton", {
+            text: "Create Information Label",
+            tooltip: "Create Information Label",
+            onAction: function (_) {
+              editor.insertContent(toInformationHtml());
+            },
+          });
+        },
       }}
     />
   );
