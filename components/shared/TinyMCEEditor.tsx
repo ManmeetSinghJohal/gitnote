4944391099b732f373b3497e18316d61e7a8d49c
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-06 09:51:49 +0100

    Add warning, error, and information label buttons to TinyMCEEditor

--- components/shared/TinyMCEEditor.tsx
+++ components/shared/TinyMCEEditor.tsx
@@ -6,7 +6,6 @@ const TinyMCEEditor: React.FC = ({field, editorRef}) => {
     <Editor
       apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
       onInit={(evt, editor) => {
-        // @ts-ignore
         editorRef.current = editor;
       }}
       onBlur={field.onBlur}
@@ -34,22 +33,45 @@ const TinyMCEEditor: React.FC = ({field, editorRef}) => {
         ],
         toolbar:
           "codesample | bold italic forecolor | alignleft aligncenter " +
-          "alignright alignjustify | bullist numlist | warningButton",
+          "alignright alignjustify | bullist numlist | warningButton | errorButton | informationButton | link image media | removeformat | fullscreen",
         content_style:
           "body { font-family:Inter; font-size:16px; background-color:#1D2032;}",
         skin: "oxide-dark",
         content_css: "dark",
         setup: (editor) => {
-             const toWarningHtml = function (date) {
-               return ( `|<div class="warning-box bg-[#20201E] border border-[#FFC700] rounded-[6px] p-2 text-white-100">WARNING LABEL</div>|`);
+             const toWarningHtml = function () {
+               return `<div class="warning-box bg-[#20201E] border border-[#FFC700] rounded-[6px] p-3.5 text-white-100">Add warning info here....</div>&#8203;`;
              };
+
+              const toErrorHtml = function () {
+                return `<div class="error-box bg-[#20201E] border border-[#FF4C3F] rounded-[6px] p-3.5 text-white-100">Add error info here....</div>&#8203;`;
+              };
+
+              const toInformationHtml = function () {
+                return `<div class="information-box bg-[#20201E] border border-[#2E3757] rounded-[6px] p-3.5 text-white-100">Add information info here....</div>&#8203;`;
+              };
+
             editor.ui.registry.addButton("warningButton", {
-              text: "Create Warning",
-              tooltip: "Create Warning",
+              text: "Create Warning Label",
+              tooltip: "Create Warning Label",
               onAction: function (_) {
-                editor.insertContent(toWarningHtml(new Date()));
+                editor.insertContent(toWarningHtml());
               },
             });
+            editor.ui.registry.addButton("errorButton", {
+              text: "Create Error Label",
+              tooltip: "Create Error Label",
+              onAction: function (_) {
+                editor.insertContent(toErrorHtml());
+              },
+            });
+             editor.ui.registry.addButton("informationButton", {
+               text: "Create Information Label",
+               tooltip: "Create Information Label",
+               onAction: function (_) {
+                 editor.insertContent(toInformationHtml());
+               },
+             });
 
         }
       }}
