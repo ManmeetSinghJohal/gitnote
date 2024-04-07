abdebde91a552f966604ce07c7b65517ea8e39b6
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-07 11:59:57 +0100

    Add EditPost component

---
+++ app/(root)/post/[postid]/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+import React from "react";
+
+import CreatePostForm from "@/components/forms/CreatePostForm";
+import { getPost } from "@/lib/actions/post.action";
+import { getTags } from "@/lib/actions/tag.actions";
+
+const EditPost = async ({ params }: { params: { postid: string } }) => {
+  const post = await getPost(params.postid);
+  const postTags = await getTags();
+// console.log("posttttt", post)
+  if (!post) return redirect("/");
+
+  return (
+    <div className="text-white-100">
+      <div className="display-1-bold mb-[30px] text-white-100 lg:mb-10">
+        Edit a Post
+      </div>
+      <div className="paragraph-3-medium mb-6 text-white-500">
+        BASIC INFORMATION
+      </div>
+      <CreatePostForm postTags={JSON.parse(postTags ?? "")} post={JSON.parse(JSON.stringify(post))}/>
+    </div>
+  );
+};
+
+export default EditPost;
