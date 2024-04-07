f06742206d887b4cb8b7cea541cb2b3447acc3d5
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-07 09:35:38 +0100

    Add CreatePost component

--- app/(root)/post/[id]/page.tsx
+++ app/(root)/post/[id]/page.tsx
@@ -9,12 +9,12 @@ const CreatePost = async () => {
   return (
     <div className="text-white-100">
       <div className="display-1-bold mb-[30px] text-white-100 lg:mb-10">
-        Create a Post
+        Edit a Post
       </div>
       <div className="paragraph-3-medium mb-6 text-white-500">
         BASIC INFORMATION
       </div>
-      <CreatePostForm postTags={JSON.parse(postTags ?? '')}/>
+      <CreatePostForm postTags={JSON.parse(postTags ?? "")} />
     </div>
   );
 };
