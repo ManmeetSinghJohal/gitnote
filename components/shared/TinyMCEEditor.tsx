902989272829321d6355a30f0987bec9672c1f3f
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-10 19:23:37 +0100

    Update warning, error, and information labels in TinyMCEEditor.tsx

--- components/shared/TinyMCEEditor.tsx
+++ components/shared/TinyMCEEditor.tsx
@@ -40,34 +40,34 @@ const TinyMCEEditor: React.FC = ({ field, editorRef, defaultValue }) => {
         content_css: "dark",
         setup: (editor) => {
           const toWarningHtml = function () {
-            return `&#8203;<div class="warning-box" style="background-color: #20201E; border: 1px solid #FFC700; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add warning info here....</div>&#8203;`;
+            return `&#8203;<div class="warning-box" style="background-color: #20201E; border: 1px solid #FFC700; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add warning here....</div>&#8203;`;
           };
 
           const toErrorHtml = function () {
-            return `&#8203;<div class="error-box" style="background-color: rgba(255, 76, 63, 0.15); border: 1px solid #FF4C3F; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add error info here....</div>&#8203;`;
+            return `&#8203;<div class="error-box" style="background-color: rgba(255, 76, 63, 0.15); border: 1px solid #FF4C3F; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add error here....</div>&#8203;`;
           };
 
           const toInformationHtml = function () {
-            return `&#8203;<div class="information-box" style="background-color: rgb(66,187,255, 0.1); border: 1px solid #42BBFF; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add information info here....</div>&#8203;`;
+            return `&#8203;<div class="information-box" style="background-color: rgb(66,187,255, 0.1); border: 1px solid #42BBFF; border-radius: 6px; margin: 20px 0; padding: 14px; color: #FFFFFF;">Add information here....</div>&#8203;`;
           };
 
           editor.ui.registry.addButton("warningButton", {
-            text: "Create Warning Label",
-            tooltip: "Create Warning Label",
+            text: "Warning",
+            tooltip: "Warning Label",
             onAction: function (_) {
               editor.insertContent(toWarningHtml());
             },
           });
           editor.ui.registry.addButton("errorButton", {
-            text: "Create Error Label",
-            tooltip: "Create Error Label",
+            text: "Error",
+            tooltip: "Error Label",
             onAction: function (_) {
               editor.insertContent(toErrorHtml());
             },
           });
           editor.ui.registry.addButton("informationButton", {
-            text: "Create Information Label",
-            tooltip: "Create Information Label",
+            text: "Information",
+            tooltip: "Information Label",
             onAction: function (_) {
               editor.insertContent(toInformationHtml());
             },
