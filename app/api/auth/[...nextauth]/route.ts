1c315b0561e70f70f49051f571b58f1d48c20ca1
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-11 13:10:51 +0100

    Refactor authentication options in route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,91 +1,9 @@
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import bycrypt from "bcryptjs";
-import { AuthOptions } from "next-auth";
+
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
 
-import clientPromise from "@/lib/mongodb";
 
-const authOptions: AuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name,
-          email: profile.email,
-        };
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-        };
-      },
-    }),
+import { authOptions } from "@/lib/auth";
 
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-        },
-      },
-      async authorize(credentials, req) {
-        if (!credentials || !credentials.email || !credentials.password)
-          return null;
-
-        const adapter = authOptions.adapter;
-
-        const user: any = await adapter?.getUserByEmail(credentials.email);
-
-        if (user) {
-          const match = await bycrypt.compare(
-            credentials.password,
-            user.password
-          );
-          return match ? user : null;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
-  adapter: MongoDBAdapter(clientPromise, {
-    databaseName: "gitnote",
-  }),
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/signin",
-  },
-  debug: process.env.NODE_ENV === "development",
-};
 
 const handler = NextAuth(authOptions);
 
