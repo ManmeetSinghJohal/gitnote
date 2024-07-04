e2a9de07c42fe02bf915104b9ca6e6e070fc91a5
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-04 23:07:52 +0100

    Add Head tag with various link tags with favicon images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 import React from "react";
 
 import { AuthProvider } from "@/components/SessionProvider";
@@ -23,6 +24,26 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={`${inter.variable} bg-black-900`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
